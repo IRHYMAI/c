5c96c51184a99fbe58a3be210185d2959e27ca9b
IRHYMAI <whjang2007@gmail.com>
2024-08-06 11:39:57 +0900

    Create foo.js
    내일 한강물 온도는 어떨까요?

---
+++ foo.js
@@ -0,0 +1,11 @@
+
+console.log("스크립트로 찍은 문자");
+let book = "자바 스크립트 기초";
+var book_price = 20000;
+var book_company = "길벗";
+console.log(book, book_price, book_company);
+
+console.log(1000);
+
+const py = "python";
+console.log(py);
